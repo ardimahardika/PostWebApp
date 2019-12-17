2cd8a8360f4626d638cccc92a541a17651fe29dc
ardi.mahardika <ardimahardika19@gmail.com>
2019-12-17 11:26:41 +0700

    init projects

---
+++ client/src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Posts from '@/components/Posts'
+import addpost from '@/components/AddPost'
+import editpost from '@/components/EditPost'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Posts',
+      component: Posts
+    },
+    {
+      path: '/posts/add',
+      component: addpost,
+      name: 'addpost'
+    },
+    {
+      path: '/posts/:id/edit',
+      component: editpost,
+      name: 'editpost'
+    }
+  ]
+})
